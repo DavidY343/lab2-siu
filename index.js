3c29bdef59d7cef6040fada67e41dfdfdbd43ed9
david <tipiraenkewa1@gmail.com>
2024-03-20 01:46:19 +0100

    Falta update Json

--- index.js
+++ index.js
@@ -3,75 +3,120 @@ const fs = require('fs');
 
 const PORT = 3000;
 
-const serveStaticFile = async (file) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, function(err, data) {
-      if(err) reject(err);
-      resolve(data);
-    });
+const serveStaticFile = async (file) =>
+{
+	return new Promise((resolve, reject) =>
+	{
+		fs.readFile(file, function(err, data)
+		{
+			if(err) reject(err);
+			resolve(data);
+		});
+	});
+}
+
+// Guardar las tareas en el archivo tasks.json
+const saveTasks = async (tasks) => {
+  // Convertir las tareas a formato JSON
+  const tasksJSON = JSON.stringify(tasks, null, 2); // null y 2 son opciones de espaciado para formatear legiblemente el JSON
+
+  // Escribir las tareas en el archivo tasks.json
+  fs.writeFile('tasks.json', tasksJSON, (err) => {
+      if (err) {
+          console.error('Error al guardar las tareas:', err);
+      } else {
+          console.log('Tareas guardadas correctamente en tasks.json');
+      }
   });
-} 
+};
 
-const sendResponse = (response, content, contentType) => {
-  response.writeHead(200, {"Content-Type": contentType});
-  response.end(content);
-}
-
-const handleRequest = async (request, response) => {
-  const url = request.url;
-
-  if(request.method === "GET"){
-    let content;
-    let contentType;
-    switch(url){
-      case "/":
-      case "/index.html":
-        content = await serveStaticFile("www/index.html");
-        contentType = "text/html";
-        break;
-      case "/script.js":
-        content = await serveStaticFile("www/script.js");
-        contentType = "text/javascript";
-        break;
-      case "/style.css":
-        content = await serveStaticFile("www/style.css");
-        contentType = "text/css";
-        break;
-      case "/tasks.json":
-        content = await serveStaticFile("tasks.json");
-        contentType = "application/json";
-        break;
-      default: 
-        content = "Ruta no v&aacutelida\r\n";
-        contentType = "text/html";
-    }
-
-     sendResponse(response, content, contentType);
-  } 
-  else if (method === "POST" && pathname === "/tasks/update") {
-    let body = '';
-    request.on('data', chunk => {
-        body += chunk.toString(); // convert Buffer to string
-    });
-    request.on('end', async () => {
-        try {
-            const tasks = JSON.parse(body);
-            await saveTasks(tasks);
-            sendResponse(response, "Tareas actualizadas correctamente", "text/plain");
-        } catch (error) {
-            console.error("Error updating tasks:", error);
-            response.writeHead(500, { "Content-Type": "text/plain" });
-            response.end("Error al actualizar las tareas");
-        }
-    });
-}else{
-     response.writeHead(405, {"Content-Type": "text/html"});
-     response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
-  }
+//enviar respuesta
+const sendResponse = (response, content, contentType) =>
+{
+	response.writeHead(200, {"Content-Type": contentType});
+	response.end(content);
 }
 
 
+// Manejar las solicitudes HTTP
+const handleRequest = async (request, response) =>
+{
+	const url = request.url;
+	if(request.method === "GET")
+	{
+		let content;
+		let contentType;
+		switch(url)
+		{
+			case "/":
+			case "/index.html":
+				content = await serveStaticFile("www/index.html");
+				contentType = "text/html";
+				break;
+			case "/script.js":
+				content = await serveStaticFile("www/script.js");
+				contentType = "text/javascript";
+				break;
+			case "/style.css":
+				content = await serveStaticFile("www/style.css");
+				contentType = "text/css";
+				break;
+			case "/tasks.json":
+				content = await serveStaticFile("tasks.json");
+				contentType = "application/json";
+				break;
+			default: 
+				content = "Ruta no v&aacutelida\r\n";
+				contentType = "text/html";
+		}
+		sendResponse(response, content, contentType);
+	}
+	/*  else if (request.method === "POST" && url === "/tasks.json") {
+	console.log("Se recibió una solicitud POST en /tasks.json");
+	let body = '';
+	request.on('data', chunk => {
+		body += chunk.toString(); // convert Buffer to string
+	});
+	request.on('end', async () => {
+		try {
+			const tasks = JSON.parse(body);
+			await saveTasks(tasks);
+			sendResponse(response, "Tareas actualizadas correctamente", "text/plain");
+		} catch (error) {
+			console.error("Error updating tasks:", error);
+			response.writeHead(500, { "Content-Type": "text/plain" });
+			response.end("Error al actualizar las tareas");
+		}
+	});
+	}*/
+	else
+	{
+		response.writeHead(405, {"Content-Type": "text/html"});
+		response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
+	}
+
+}
+
+// Crear un servidor HTTP que maneje las solicitudes con la función handleRequest
 const server = http.createServer(handleRequest);
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
+
+
+
+
+
+/*
+// Manejar la solicitud POST para actualizar la lista de tareas en el servidor
+app.post('/tasklist/update', async (req, res) => {
+  try {
+    const tasks = req.body; // Obtener la lista de tareas del cuerpo de la solicitud
+    await updateTasks(tasks); // Actualizar las tareas en el servidor
+    res.sendStatus(200); // Enviar una respuesta de estado OK al cliente
+  } catch (error) {
+    console.error('Error updating tasks on server:', error);
+    res.sendStatus(500); // Enviar una respuesta de estado Internal Server Error al cliente en caso de error
+  }
+});*/
--- www/script.js
+++ www/script.js
@@ -1,164 +1,219 @@
 let taskList = [];
 
-const loadTasks = async () => {
-    try {
-        const response = await fetch("/tasks.json", { method: "GET" });
-        if (!response.ok) {
-            throw new Error("Failed to fetch tasks");
-        }
-        const tasks = await response.json();
-        taskList.push(...tasks);
-        updateUI(taskList);
-    } catch (error) {
-        console.error("Error loading tasks:", error);
-    }
+// Esta funcion carga las tareas desde el servidor
+const loadTasks = async () =>
+{
+	try
+	{
+		const response = await fetch("/tasks.json", { method: "GET" });
+		if (!response.ok)
+		{
+			throw new Error("Failed to fetch tasks");
+		}
+		const tasks = await response.json();
+		taskList.push(...tasks);
+		updateUI(taskList);
+	}
+	catch (error)
+	{
+		console.error("Error loading tasks:", error);
+	}
 }
 
-const add = async () => {
-    try {
-        const taskNameInput = document.getElementById("task-name");
-        const taskName = taskNameInput.value.trim();
-        if (taskName === "") {
-            return;
-        }
-        const newTask = { id: Date.now(), title: taskName, done: false };
-        taskList.push(newTask);
-        updateUI(taskList);
-        taskNameInput.value = "";
-        await saveTasks();
-    } catch (error) {
-        console.error("Error adding task:", error);
-    }
+// Esta funcion agrega una tarea a la lista
+const add = async () =>
+{
+	try
+	{
+		const taskNameInput = document.getElementById("task-name");
+		const taskName = taskNameInput.value.trim();
+		if (taskName === "")
+		{
+			return;
+		}
+		const lastTaskId = taskList.length > 0 ? taskList[taskList.length - 1].id : 0;
+		const newTask = { id: lastTaskId + 1, title: taskName, done: false };
+		console.log(taskList);
+		taskList.push(newTask);
+		console.log(taskList);
+		updateUI(taskList);
+		taskNameInput.value = "";
+		//await saveTasks();
+	}
+	catch (error)
+	{
+		console.error("Error adding task:", error);
+	}
 }
 
-const remove = async (taskId) => {
-    taskId = parseInt(taskId);
-    console.log("ID de tarea a eliminar:", taskId);
-    const index = taskList.findIndex(task => task.id === taskId);
-    if (index !== -1) {
-        taskList.splice(index, 1);
-        updateUI(taskList);
-    } else {
-        console.error(`No se encontró ninguna tarea con el ID ${taskId}`);
-    }
+// Esta funcion elimina una tarea de la lista
+const remove = async (taskId) =>
+{
+	try
+	{
+		taskId = parseInt(taskId);
+		const index = taskList.findIndex(task => task.id === taskId);
+		if (index !== -1)
+		{
+			taskList.splice(index, 1);
+			updateUI(taskList);
+			//await saveTasks();
+		}
+	}
+	catch (error)
+	{
+		console.error("Error removing task:", error);
+	}
 };
 
-const toggleDone = async (taskId, taskElement) => {
-	const taskIndex = taskList.findIndex(task => task.id === taskId);
-    if (taskIndex !== -1) {
-        taskList[taskIndex].done = !taskList[taskIndex].done;
-        updateUI(taskList);
+// Esta funcion cambia el estado de una tarea de completada a no completada y viceversa
+const toggleDone = async (taskId, taskElement) =>
+{
+    try
+	{
+        taskId = parseInt(taskId);
+        const taskIndex = taskList.findIndex(task => task.id === taskId);
+        if (taskIndex !== -1)
+		{
+            taskList[taskIndex].done = !taskList[taskIndex].done;
+            updateUI(taskList);
+			//await saveTasks();
+        }
+    }
+	catch (error)
+	{
+        console.error("Error toggling task done state:", error);
     }
 }
 
-const saveTasks = async () => {
-    try {
-        const response = await fetch("/tasks/update", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(taskList)
-        });
-        if (!response.ok) {
-            throw new Error("Failed to save tasks");
-        }
-    } catch (error) {
-        console.error("Error saving tasks:", error);
-    }
+// Esta funcion guarda las tareas en el servidor, no funciona por el momento
+const saveTasks = async () =>
+{
+	try
+	{
+		const response = await fetch("/tasks.json",
+		{
+			method: "POST",
+			headers:
+			{
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(taskList)
+		});
+		if (!response.ok)
+		{
+			throw new Error("Failed to save tasks");
+		}
+	}
+	catch (error)
+	{
+		console.error("Error saving tasks:", error);
+	}
 }
 
-const updateUI = (taskList) => {
-    const taskListElement = document.querySelector("#task-list");
-    taskListElement.innerHTML = "";
-    taskList.forEach(task => {
-        const listItem = document.createElement("li");
-        listItem.textContent = task.title;
-        listItem.dataset.taskId = task.id;
-        if (task.done) {
-            listItem.classList.add("completed");
-        }
-        taskListElement.appendChild(listItem);
-    });
+// Esta funcion actualiza la interfaz de usuario con las tareas actuales
+const updateUI = (taskList) =>
+{
+	const taskListElement = document.querySelector("#task-list");
+	taskListElement.innerHTML = "";
+	taskList.forEach(task =>
+	{
+		const listItem = document.createElement("li");
+		listItem.textContent = task.title;
+		listItem.dataset.taskId = task.id;
+		if (task.done)
+		{
+			listItem.classList.add("completed");
+		}
+		taskListElement.appendChild(listItem);
+	});
 }
 
 document.addEventListener("DOMContentLoaded", loadTasks);
 document.getElementById("fab-add").addEventListener("click", add);
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const taskListElement = document.getElementById('task-list');
-    console.log(taskListElement);
-    taskListElement.addEventListener('touchstart', handleTouchStart, false);
-    taskListElement.addEventListener('touchmove', handleTouchMove, false);
-	taskListElement.addEventListener('touchend', handleTouchEnd, false);
-    let startX = null;
+// Tareas de desplazamiento sobre los elementos de la lista
+let timer = null;
+const taskListElement = document.getElementById('task-list');
+taskListElement.addEventListener('touchstart', handleTouchStart, false);
+taskListElement.addEventListener('touchmove', handleTouchMove, false);
+taskListElement.addEventListener('touchend', handleTouchEnd, false);
+let startX = null;
 
-    function handleTouchStart(event) {
-        const firstTouch = event.touches[0];
-        startX = firstTouch.clientX;
-        // Iniciar temporizador cuando se toca la tarea
-        timer = setTimeout(() => {
-            const taskElement = event.target.closest('li');
-            if (taskElement) {
-				const taskId = taskElement.dataset.taskId;
-				if (taskId) {
-					toggleDone(parseInt(taskId), taskElement);
-            	}
+// manejar el inicio del toque (iniciar temporizador) y iniciar la posibilidad de desplazamiento
+function handleTouchStart(event)
+{
+	const firstTouch = event.touches[0];
+	startX = firstTouch.clientX;
+	timer = setTimeout(() => 
+	{
+		const taskElement = event.target.closest('li');
+		if (taskElement)
+		{
+			const taskId = taskElement.dataset.taskId;
+			if (taskId)
+			{
+				toggleDone(parseInt(taskId), taskElement);
 			}
-        }, 2000); // 2000 milisegundos (2 segundos)
-    }
+		}
+	}, 2000);
+}
 
-    
-    function handleTouchEnd() {
-        clearTimeout(timer); // Limpiar el temporizador cuando se levanta el dedo
-    }
-    
-    function handleTouchMove(event) {
-        if (!startX) {
-            return;
-        }
+// Manejar el final del toque (cancelar el temporizador si no se ha completado)
+function handleTouchEnd()
+{
+	clearTimeout(timer);
+}
 
-        const touch = event.touches[0];
-        const deltaX = touch.clientX - startX;
-
-        // Si el desplazamiento horizontal es mayor que el vertical
-        if (deltaX > 7) {
-            // Determinar si el desplazamiento fue hacia la derecha
-            const closestTask = event.target.closest('li');
-            
-            if (closestTask) {
-                closestTask.classList.add('slide-out');
-                // Obtener el ID de la tarea a eliminar
-                setTimeout(() => {
-                    // Obtener el ID de la tarea a eliminar
-                    const taskId = closestTask.dataset.taskId;
-                    if (taskId) {
-                        remove(taskId);
-                    }
-                }, 2000); // Espera 500 milisegundos (0.5 segundos)
-            }
-        }
-
-        // Restablecer las coordenadas de inicio
-        startX = null;
-    }
-});
-
+// Manejar el movimiento del toque (eliminar la tarea si se desplaza hacia la derecha)
+function handleTouchMove(event)
+{
+	if (!startX)
+	{
+		return;
+	}
+	const touch = event.touches[0];
+	const deltaX = touch.clientX - startX;
+	if (deltaX > 7)
+	{
+		const closestTask = event.target.closest('li');
+		if (closestTask)
+		{
+			closestTask.classList.add('slide-out');
+			setTimeout(() =>
+			{
+				const taskId = closestTask.dataset.taskId;
+				if (taskId)
+				{
+					remove(taskId);
+				}
+			}, 2000);
+		}
+	}
+	startX = null;
+}
 
 
-// Obtener el botón de información y el cuadro emergente
 const infoButton = document.getElementById('info-button');
 const infoPopup = document.getElementById('info-popup');
 
 // Mostrar el cuadro emergente al hacer clic en el botón
-infoButton.addEventListener('click', (event) => {
+infoButton.addEventListener('click', (event) =>
+{
 	event.stopPropagation(); // Evitar la propagación del evento
-	infoPopup.style.display = 'block';
-  })
+	if (infoPopup.style.display === 'block')
+	{
+		infoPopup.style.display = 'none';
+	}
+	else
+	{
+		infoPopup.style.display = 'block';	
+	}
+})
+
 // Ocultar el cuadro emergente al hacer clic fuera de él
-window.addEventListener('click', (event) => {
-  if (event.target !== infoButton && !infoPopup.contains(event.target)) {
+window.addEventListener('click', () =>
+{
     infoPopup.style.display = 'none';
-  }
 });
